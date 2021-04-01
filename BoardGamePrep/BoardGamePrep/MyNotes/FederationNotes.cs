c1ead78e89610612ebb1e683fcf3f788e68c36e4
Swiftyfast <jdavidswift@gmail.com>
2021-04-01 09:13:34 -0500

    Basic INfo

---
+++ BoardGamePrep/BoardGamePrep/MyNotes/FederationNotes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGamePrep.MyNotes
+{
+    class FederationNotes
+    {
+        static void Main(string[] args)
+        {
+            //Here is the basic notes for the federation gameplay
+
+            //Put out three types of cards to start the game
+            //THREAT CARDS - these show the longterm threat that is going out against the humans. Could be Civil War, Khan, Alien Invasion, Robot Uprising, Interdimensional Assault
+
+            //PROJECT CARDS - these are goals to be accomplished that will grant Prestige to the players. Could be completion of combat defense system, a space station construction project, or a portal network.
+
+            //PATH CARDS - Draw one per game to change to board at the beginning of the game
+
+        }
+    }
+}
