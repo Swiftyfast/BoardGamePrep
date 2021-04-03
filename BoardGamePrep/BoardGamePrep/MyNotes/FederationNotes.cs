0482f17837d3e8980fc79503ff9a362271dbfc2d
Swiftyfast <jdavidswift@gmail.com>
2021-04-03 12:11:17 -0500

    Entered Notes

--- BoardGamePrep/BoardGamePrep/MyNotes/FederationNotes.cs
+++ BoardGamePrep/BoardGamePrep/MyNotes/FederationNotes.cs
@@ -10,23 +10,24 @@ namespace BoardGamePrep.MyNotes
     {
         //static void Main(string[] args)
         //{
-            //Here is the basic notes for the federation gameplay
+        //Here is the basic notes for the federation gameplay
 
-            //Put out three types of cards to start the game
-            //THREAT CARDS - these show the longterm threat that is going out against the humans. Could be Civil War, Khan, Alien Invasion, Robot Uprising, Interdimensional Assault, Giant Space Shark
+        //Put out three types of cards to start the game
+        //THREAT CARDS - these show the longterm threat that is going out against the humans. Could be Civil War, Khan, Alien Invasion, Robot Uprising, Interdimensional Assault, Giant Space Shark
 
-            //PROJECT CARDS - these are goals to be accomplished that will grant Prestige to the players. Could be completion of combat defense system, a space station construction project, or a portal network.
+        //PROJECT CARDS - these are goals to be accomplished that will grant Prestige to the players. Could be completion of combat defense system, a space station construction project, or a portal network.
 
-            //PATH CARDS - Draw one per game to change to board at the beginning of the game
+        //PATH CARDS - Draw one per game to change to board at the beginning of the game
 
-            //SHIP CARDS
-            //CAPTAIN CARDS
-            //BONUSES
-            //HULL DAMAGE CARDS
-            //MIDNIGHT EFFECT CARDS
+        //SHIP CARDS
+        //CAPTAIN CARDS
+        //BONUSES
+        //HULL DAMAGE CARDS
+        //MIDNIGHT EFFECT CARDS
 
-           //ENCOUNTER CARDS
-            
+        //ENCOUNTER CARDS
+        //Check drive documentations to see progress
+
         //}
     }
 }
