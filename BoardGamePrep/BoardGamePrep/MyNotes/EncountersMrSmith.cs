5c21483bc198556ac419a8eb8a72ded2b396ad2b
Swiftyfast <jdavidswift@gmail.com>
2021-04-06 10:18:31 -0500

    Making basic encounter formats

---
+++ BoardGamePrep/BoardGamePrep/MyNotes/EncountersMrSmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGamePrep.MyNotes
+{
+    class EncountersMrSmith
+    {
+        //Template
+
+        //Trappist
+        //blah blah blah
+        //Valleytown
+        //blah blah blah
+        //Unexplored Frontier
+        //blah blah blah
+
+    }
+}
