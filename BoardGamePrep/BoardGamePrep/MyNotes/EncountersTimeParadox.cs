9c9860b8cc61a7a4b8b077940e91ba9f5b28d259
Swiftyfast <jdavidswift@gmail.com>
2021-06-10 10:18:46 -0500

    Some time paradox ideas

--- BoardGamePrep/BoardGamePrep/MyNotes/EncountersTimeParadox.cs
+++ BoardGamePrep/BoardGamePrep/MyNotes/EncountersTimeParadox.cs
@@ -8,5 +8,14 @@ namespace BoardGamePrep.MyNotes
 {
     class EncountersTimeParadox
     {
+        //Ok so these are using mostly star trek encounters but also some doctor who probably.
+
+        //The crew are children
+        //You have to think in 3 different timelines at once(All good things...)
+
+        //Rethinking choices that led you to get stabbed
+
+        //Changing the past
+
     }
 }
