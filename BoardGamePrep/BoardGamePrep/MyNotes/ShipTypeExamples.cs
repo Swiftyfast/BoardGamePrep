809e4ad3809fa3aedd8b7ccf516b89dff27161bd
Swiftyfast <jdavidswift@gmail.com>
2021-06-14 14:10:53 -0500

    Added examples.

---
+++ BoardGamePrep/BoardGamePrep/MyNotes/ShipTypeExamples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGamePrep.MyNotes
+{
+    class ShipTypeExamples
+    {
+        // One would be ship captain combo
+
+        //Two would be ship matched with captain
+
+        //Three would be captain and ship archetype that gets a free start object
+    }
+}
