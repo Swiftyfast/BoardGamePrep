42222e08b0d94b65aee3991533e2aeae8cc66302
Swiftyfast <jdavidswift@gmail.com>
2021-06-18 14:39:21 -0500

    Made a new class

---
+++ BoardGamePrep/BoardGamePrep/MyNotes/OtherItems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGamePrep.MyNotes
+{
+    class OtherItems
+    {
+        //A city building game
+        //like Majesty
+        //Or Steampunk
+        //OR scifi?
+    }
+}
